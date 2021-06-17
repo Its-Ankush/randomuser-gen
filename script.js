cb6165359feb501f67ef289fd9b8e4eaa9c25f71
Senpai <68053631+Its-Ankush@users.noreply.github.com>
2021-06-17 22:12:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+let request = new XMLHttpRequest();
+let button = document.querySelector(".btn");
+let output = document.querySelector(".output");
+
+let dataLoader = function () {
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+      let data = JSON.parse(request.responseText);
+      let first = data.results[0].name.first,
+        last = data.results[0].name.last,
+        pic = data.results[0].picture.large;
+      let html = `<br><br><img src=${pic}><br>${first} ${last}`;
+      output.innerHTML = html;
+    }
+  };
+  request.open("GET", "https://randomuser.me/api", true);
+  request.send();
+};
+button.addEventListener("click", dataLoader);
+// console.log(request);
